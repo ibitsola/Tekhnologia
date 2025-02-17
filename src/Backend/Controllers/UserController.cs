6cfee6808a2965efdca128a19863d41a5669a92c
Panagiota Ira Bitsola <panagiotairabitsola@tfl.gov.uk>
2025-02-17 14:45:24 +0000

    Remove usless using statement in usercontroller

--- src/Backend/Controllers/UserController.cs
+++ src/Backend/Controllers/UserController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
-using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 
 namespace Backend.Controllers
